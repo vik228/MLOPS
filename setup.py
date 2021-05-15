d851e77ba6b5ff2af4c658022228d5480bd5a231
Vikas Pandey <vikaspandey@Vikass-MacBook-Pro.local>
2021-05-16 04:50:25 +0530

    added testing

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name="src",
+    version="0.0.1",
+    description="Its a wine Q package",
+    author="vik228",
+    packages=find_packages(),
+    license="MIT"
+)
